157bd30cb3d274df931046d4c732286c3390d086
poiqwe <38466541+poiqwe96@users.noreply.github.com>
2022-07-08 18:47:35 +0900

    캐시샵과 이벤트맵 클래스추가

---
+++ Source/MyJrpg/Widgets/CashShop/WidgetCashShopCanvas.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "WidgetCashShopCanvas.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYJRPG_API UWidgetCashShopCanvas : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
